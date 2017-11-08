8be4b594332c9770d11a64fc29f921ee155f2200
Juananto11 <juananto11@hotmail.com>
2017-11-08 13:29:43 -0600

    retoques en las  media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-onscroll = () => {
+/*onscroll = () => {
 	const titulo = document.getElementById('titulo');
 		if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
 			titulo.style.fontSize = '2em'
@@ -7,5 +7,4 @@ onscroll = () => {
 			titulo.style.fontSize = '3em'
 		}
 }
-
-console.log(screen.width);
+*/
