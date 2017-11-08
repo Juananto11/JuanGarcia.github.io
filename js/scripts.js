66b3dc7743445ce22836986bdeddaaca808c4791
Juananto11 <juananto11@hotmail.com>
2017-11-08 10:31:29 -0600

    Se agrego viewport

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 onscroll = () => {
 	const titulo = document.getElementById('titulo');
 
-	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+	if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
 		titulo.style.fontSize = '2em'
 	} else {
 		titulo.style.fontSize = '3em'
