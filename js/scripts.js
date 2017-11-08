b8b0fbdfb339d9d8dbb9f004d4b084b3b8eb7b51
Juananto11 <juananto11@hotmail.com>
2017-11-08 12:08:29 -0600

    se agregaron media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 onscroll = () => {
 	const titulo = document.getElementById('titulo');
-
-	if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
-		titulo.style.fontSize = '2em'
-	} else {
-		titulo.style.fontSize = '3em'
-	}
+		if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+			titulo.style.fontSize = '2em'
+		}
+		else {
+			titulo.style.fontSize = '3em'
+		}
 }
+
+console.log(screen.width);
